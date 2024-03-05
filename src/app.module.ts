fe976dfe9d47ddf41ee407097ce53a190eef0945
Lakshya Saini <saini.lakshya1998@gmail.com>
2024-03-05 19:40:50 +0530

    add endpoints in app controller to interact with cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { UserModule } from './user/user.module';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      max: 100,
+      ttl: 0,
+      isGlobal: true,
+      store: redisStore,
+      host: '172.17.0.2',
+      port: 6379,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
